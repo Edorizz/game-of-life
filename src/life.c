d3480a5930da259a73f313d578ebd4f55cb3794d
Edgar Mendoza <edorizz_99@hotmail.com>
2017-09-19 21:30:16 -0700

    Add support for multiple colored cells.
    - Can now specify the number of cell colors with the '-c colors'
      command line argument (max 3).
    
    - Disable border wrapping with '-b'.

--- src/life.c
+++ src/life.c
@@ -10,40 +10,37 @@
 #include <dmatrix.h>
 
 /* Game variables */
-#define TIME_STEP	0.08
+#define TIME_STEP	0.04
 #define TIME_STEP_DIFF	0.02
 
-/* Cell states */
-#define CELL_DEAD	0
-#define CELL_ALIVE	1
-
-/* Grids */
+/* Grid */
 #define TMP_GRID	0
 #define REAL_GRID	1
-#define GRID_W		128
-#define GRID_H		128
-
-/* Colors */
-#define WHITE		0
-#define BLACK		1
+#define GRID_W		320
+#define GRID_H		180
+#define MAX_CELL_TYPES	3
 
 /* Window dimensions */
-#define WINDOW_W	768
-#define WINDOW_H	768
+#define WINDOW_W	1280
+#define WINDOW_H	720
 
 /* Game state flags */
 #define QUIT		0
 #define PAUSE		1
 #define DRAW		2
+#define BORDER		3
 
 /* Macro fucntions */
 #define BIT(n)		(1 << (n))
+#define MIN(a, b)	((a) < (b) ? (a) : (b))
+#define MAX(a, b)	((a) > (b) ? (a) : (b))
 
 struct life {
 	/* [Grid] */
 	int *grid;
 	int *grid_tmp;
 	int grid_h, grid_w;
+	int cell_types;
 
 	/* [Simulation flags] */
 	double time_step;
@@ -51,11 +48,19 @@ struct life {
 	uint8_t flags;
 };
 
-const uint8_t colors[][3] = { { 0, 0, 0 }, { 58, 168, 220 } };
+const uint8_t colors[][3] = { { 0, 0, 0 },
+			      { 58, 168, 220 },
+			      { 220, 20, 68 },
+			      { 220, 168, 68 } };
 
 int
 get_cell(struct life *conway, int y, int x, int grid)
 {
+	if (conway->flags & BIT(BORDER) &&
+	    ((y < 0 || y >= conway->grid_h || x < 0 || x >= conway->grid_w))) {
+		return 0;
+	}
+
 	y += conway->grid_h;
 	x += conway->grid_w;
 
@@ -70,6 +75,11 @@ get_cell(struct life *conway, int y, int x, int grid)
 void
 set_cell(struct life *conway, int y, int x, int state, int grid)
 {
+	if (conway->flags & BIT(BORDER) &&
+	    ((y < 0 || y >= conway->grid_h || x < 0 || x >= conway->grid_w))) {
+		return;
+	}
+
 	if (grid) {
 		conway->grid[(y % conway->grid_h) * conway->grid_w + (x % conway->grid_w)] = state;
 
@@ -79,35 +89,48 @@ set_cell(struct life *conway, int y, int x, int state, int grid)
 }
 
 int
-neighbour_cnt(struct life *conway, int y, int x)
+neighbour_cnt(struct life *conway, int y, int x, int *hi_cnt)
 {
-	int i, j, n;
+	int i, j, n, cell;
+	uint8_t n_cnt[conway->cell_types];
+
+	for (i = 0; i != conway->cell_types; ++i) {
+		n_cnt[i] = 0;
+	}
 
 	n = 0;
-
 	for (i = -1; i != 2; ++i) {
 		for (j = -1; j != 2; ++j) {
-
-			if (get_cell(conway, y + i, x + j, TMP_GRID) == CELL_ALIVE) {
+			if ((cell = get_cell(conway, y + i, x + j, TMP_GRID)) != 0) {
+				++n_cnt[cell - 1];
 				++n;
 			}
 		}
 	}
 
-	return n - get_cell(conway, y, x, TMP_GRID);
+	*hi_cnt = 0;
+	for (i = 0; i != conway->cell_types; ++i) {
+		if (n_cnt[i] > n_cnt[*hi_cnt]) {
+			*hi_cnt = i;
+		}
+	}
+
+	++*hi_cnt;
+
+	return n - (get_cell(conway, y, x, TMP_GRID) ? 1 : 0);
 }
 
 int
 update_cell(struct life *conway, int y, int x)
 {
-	int nb;
+	int nb, hi_cnt;
 
-	nb = neighbour_cnt(conway, y, x);
+	nb = neighbour_cnt(conway, y, x, &hi_cnt);
 	if (get_cell(conway, y, x, TMP_GRID)) {
-		return nb < 2 || nb > 3 ? CELL_DEAD : CELL_ALIVE;
+		return nb < 2 || nb > 3 ? 0 : hi_cnt;
 
 	} else {
-		return nb == 3 ? CELL_ALIVE : CELL_DEAD;
+		return nb == 3 ? hi_cnt : 0;
 	}
 }
 
@@ -141,20 +164,8 @@ randomize_grid(struct life *conway)
 	int i;
 
 	for (i = 0; i != conway->grid_h * conway->grid_w; ++i) {
-		conway->grid[i] = rand() % 2;
+		conway->grid[i] = rand() % (1 + conway->cell_types);
 	}
-}
-
-void
-init_life(struct life *conway)
-{
-	conway->grid = malloc(conway->grid_h * conway->grid_w * sizeof(int));
-	conway->grid_tmp = malloc(conway->grid_h * conway->grid_w * sizeof(int));
-	conway->flags |= BIT(DRAW);
-	conway->timer = glfwGetTime();
-	conway->time_step = TIME_STEP;
-
-	randomize_grid(conway);
 }
 
 void
@@ -201,29 +212,74 @@ main(int argc, char **argv)
 {
 	struct life conway;
 	struct dot_matrix dm;
-	uint8_t scr_buf[GRID_H][GRID_W][3];
+	uint8_t scr_buf[GRID_H * GRID_W][3];
 	double x, y;
-	int i, j, ix, iy;
+	int i, ix, iy, win_w, win_h, arg;
 
+	/* Random seed */
 	srand(time(NULL));
 
-	/* Create window */
-	memset(scr_buf[0][0], 2, sizeof scr_buf);
-	dm.scr_buf_h = GRID_H;
+	/* Default values */
+	win_w = WINDOW_W;
+	win_h = WINDOW_H;
 	dm.scr_buf_w = GRID_W;
-	dm.scr_buf = scr_buf[0][0];
-	create_matrix(&dm, WINDOW_W, WINDOW_H);
+	dm.scr_buf_h = GRID_H;
+
+	conway.flags |= BIT(DRAW) | BIT(PAUSE);
+	conway.timer = glfwGetTime();
+	conway.time_step = TIME_STEP;
+	conway.cell_types = 1;
+
+	for (i = 1; i != argc; ++i) {
+		if (argv[i][0] == '-') {
+			if (i < argc - 1) {
+				arg = atoi(argv[i + 1];
+			}
+
+			switch (argv[i][1]) {
+			case 'x':
+				win_w = arg;
+				break;
+
+			case 'y':
+				win_h = arg;
+				break;
+				
+			case 'w':
+				dm.scr_buf_w = arg;
+				break;
+
+			case 'h':
+				dm.scr_buf_h = arg;
+				break;
+
+			case 'c':
+				conway.cell_types = MAX(MIN(arg, MAX_CELL_TYPES), 1);
+				break;
+
+			case 'b':
+				conway.flags |= BIT(BORDER);
+				break;
+			}
+		}
+	}
+
+	/* Create window */
+	memset(scr_buf[0], 0, sizeof scr_buf);
+	dm.scr_buf = scr_buf[0];
+	create_matrix(&dm, win_w, win_h);
+
+	/* Initialize game of struct life */
+	conway.grid_h = dm.scr_buf_h;
+	conway.grid_w = dm.scr_buf_w;
+	conway.grid = malloc(conway.grid_h * conway.grid_w * sizeof(int));
+	conway.grid_tmp = malloc(conway.grid_h * conway.grid_w * sizeof(int));
+	randomize_grid(&conway);
 
 	/* Some GLFW stuff */
 	glfwSetWindowUserPointer(dm.win, &conway);
 	glfwSetKeyCallback(dm.win, key_callback);
 	glfwSwapInterval(1);
-
-	/* Initialize game of struct life */
-	memset(&conway, 0, sizeof(conway));
-	conway.grid_h = GRID_H;
-	conway.grid_w = GRID_W;
-	init_life(&conway);
 
 	while (!glfwWindowShouldClose(dm.win) && !(conway.flags & BIT(QUIT))) {
 		glfwPollEvents();
@@ -233,8 +289,8 @@ main(int argc, char **argv)
 			conway.flags |= BIT(DRAW);
 			
 			glfwGetCursorPos(dm.win, &x, &y);
-			ix = (x / (double)WINDOW_W) * GRID_W;
-			iy = (y / (double)WINDOW_H) * GRID_H;
+			ix = (x / (double)win_w) * conway.grid_w;
+			iy = (y / (double)win_h) * conway.grid_h;
 			
 			conway.grid[iy * GRID_W + ix] = 1;
 
@@ -242,8 +298,8 @@ main(int argc, char **argv)
 			conway.flags |= BIT(DRAW);
 			
 			glfwGetCursorPos(dm.win, &x, &y);
-			ix = (x / (double)WINDOW_W) * GRID_W;
-			iy = (y / (double)WINDOW_H) * GRID_H;
+			ix = (x / (double)win_w) * conway.grid_w;
+			iy = (y / (double)win_h) * conway.grid_h;
 			
 			conway.grid[iy * GRID_W + ix] = 0;
 		}
@@ -259,13 +315,8 @@ main(int argc, char **argv)
 
 		/* Update screen buffer only if necessary */
 		if (conway.flags & BIT(DRAW)) {
-			for (i = 0; i != GRID_H; ++i) {
-				for (j = 0; j != GRID_W; ++j) {
-					/*
-					scr_buf[i][j][1] = 255 * conway.grid[i * GRID_W + j];
-					*/
-					memcpy(scr_buf[i][j], &colors[conway.grid[i * GRID_W + j]], sizeof (uint8_t) * 3);
-				}
+			for (i = 0; i != conway.grid_w * conway.grid_h; ++i) {
+				memcpy(scr_buf[i], &colors[conway.grid[i]], sizeof (uint8_t) * 3);
 			}
 			
 			update_matrix(&dm);
